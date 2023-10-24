ec6f9710332ce6c2dedab0a29e980b520b127f31
Craig Hair <craig.hair@gmail.com>
2023-10-24 19:21:21 +1100

    Update GetMDT.ps1
    MS changed the way the DL button for MDT works...

--- GetMDT.ps1
+++ GetMDT.ps1
@@ -12,8 +12,6 @@ $MyApp = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uni
         Where-Object {$_.DisplayName -like "*$SoftwareName*" -or $_.DisplayName -like "*$SoftwareALTName*"} |
             Select-Object -Property DisplayName, UninstallString, DisplayVersion
 
-
-
 ForEach ($App in $MyApp) {
 
 
@@ -37,8 +35,9 @@ Write-Host "MDT ADK link is $ADKURL"
 Start-BitsTransfer -Source $ADKURL -Destination "$Location\ADK.exe" -Priority high
 
 # ADK INSTALL
-
-Write-Host "Installing ADK"
+# For a complete list of all available features run $Location\ADK.exe /list
+Write-Host "Installing ADK
+    For a complete list of all available features run $Location\ADK.exe /list"
 Start-Process $Location\ADK.exe -ArgumentList "/features OptionId.DeploymentTools OptionId.ICDConfigurationDesigner OptionId.ImagingAndConfigurationDesigner OptionId.UserStateMigrationTool /q" -Wait
 
 
@@ -60,11 +59,26 @@ Start-Process $Location\ADKPE.exe -ArgumentList "/features + /q" -Wait
 
 # MDT Get URL
 
-$pagelink = ((Invoke-WebRequest -UseBasicParsing -Uri "https://aka.ms/mdtdownload").Links | 
-Where-Object {($_.href -like "*confirmation.aspx?id=*") -and ($_.class -like "*download-button*")}).href
+# $pagelink = ((Invoke-WebRequest -UseBasicParsing -Uri "https://aka.ms/mdtdownload").Links | 
+# Where-Object {($_.href -like "*confirmation.aspx?id=*") -and ($_.class -like "*download-button*")}).href
 
-$MDTURL = ((Invoke-WebRequest -UseBasicParsing -Uri "https://www.microsoft.com/en-us/download/$pagelink").Links | 
-Where-Object {$_.href -like "*MicrosoftDeploymentToolkit*x64.msi*"}).href | Select-Object -first 1
+# $MDTURL = ((Invoke-WebRequest -UseBasicParsing -Uri "https://www.microsoft.com/en-us/download/$pagelink").Links | 
+# Where-Object {$_.href -like "*MicrosoftDeploymentToolkit*x64.msi*"}).href | Select-Object -first 1
+
+# Thanks Nathan Stockton for finding this one when MS changed it....
+
+$response = Invoke-WebRequest -UseBasicParsing -Uri "https://aka.ms/mdtdownload"
+$html = $response
+
+$findDownloadObject = [regex]::Match($html.Content, '"downloadFile":(.*?)]')
+$downloadObject = ($findDownloadObject.Groups[1].Value+"]") | ConvertFrom-Json
+
+if ($downloadObject.url) {
+    $MDTURL = $downloadObject.url
+    Write-Host $MDTURL
+} else {
+    Write-Host "Couldn't find download location"
+}
 
 # MDT Download
 
